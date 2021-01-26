2f92e0b0b3ff33819ae25ca706d76efffb2f8fff
elektrababian <elektra.babian@generalassemb.ly>
2021-01-26 16:22:38 -0500

    added nav and home page

---
+++ src/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from './Nav'
+import './Home.css'
+
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <Nav />
+            <h1>Elektra Babian</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/Components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Nav.css'
+
+const Nav = () => {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Projects</li>
+                <li>Contact</li>
+                <li>Resume</li>
+            </ul>
+            
+        </nav>
+    );
+};
+
+export default Nav;
