53de8ca8714930416b275169eb56e1ad939505fa
elektrababian <elektra.babian@generalassemb.ly>
2021-01-26 18:50:03 -0500

    added a footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Footer.css'
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <a href="https://github.com/ebabian" target="_blank">
+                <img id="social" src="https://img.icons8.com/material-sharp/96/000000/github.png"/>
+            </a>
+            <a href="https://www.linkedin.com/in/elektrababian/" target="_blank">
+                <img id="social" src="https://img.icons8.com/ios-filled/100/000000/linkedin.png"/>
+            </a>
+        </div>
+    );
+};
+
+export default Footer;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Nav from './Nav'
 import './Home.css'
-
+import Footer from './Footer'
 
 const Home = () => {
     return (
@@ -9,6 +9,7 @@ const Home = () => {
             <Nav />
             <h1 className="fade-in">Elektra Babian</h1>
             <h2 className="fade-in">Software Engineer</h2>
+            <Footer />
         </div>
     );
 };
