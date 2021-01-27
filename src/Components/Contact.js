964969b205de40e43875a3810b84b1f9f384eb6e
ebabian <babianelektra@gmail.com>
2021-01-27 14:45:34 -0500

    fixed responsive styling for contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -11,6 +11,11 @@ const Contact = () => {
                 <img id="contactImg" src="https://i.imgur.com/WRsSm98.jpg"/>
             </div>
             <div className="contact-bubble">
+                <p id="contact-p">To contact Elektra you can reach her with the email listed below. Thank you!</p>
+                <p id="email-p">babianelektra@gmail.com</p>
+            </div>
+            <div className="send-div">
+                <a id="send-btn" href="mailto: babianelektra@gmail.com">SEND</a>
             </div>
             <Footer />
         </div>
