255cf8f4b4e6cdc6487992f44583a1efa0079160
ebabian <babianelektra@gmail.com>
2021-01-27 14:11:28 -0500

    added div bubble to contact

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Nav from './Nav';
+import Footer from './Footer'
 import './Contact.css'
 
 const Contact = () => {
@@ -9,6 +10,9 @@ const Contact = () => {
             <div className="imgDiv">
                 <img id="contactImg" src="https://i.imgur.com/WRsSm98.jpg"/>
             </div>
+            <div className="contact-bubble">
+            </div>
+            <Footer />
         </div>
     );
 };
