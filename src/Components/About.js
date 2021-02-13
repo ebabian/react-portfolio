01eceeb79cf8400ab9b9cdce87e4e7956a416979
ebabian <babianelektra@gmail.com>
2021-02-12 21:32:54 -0500

    Restructuring About page

--- src/Components/About.js
+++ src/Components/About.js
@@ -14,8 +14,9 @@ const About = () => {
             <div className="about-container">
                 <h1 id="about-h1">Hi, I'm Elektra.<img id="hand-icon" alt="Hand Wave" src="https://img.icons8.com/ios-filled/100/000000/so-so.png"/></h1>
                 <h4 id="about-desc-h4">I'm a full-stack software engineer and digital creator from Brooklyn, New York.</h4>
-
-                {/* <div className="jobs-container"> 
+            </div>
+                <div className="jobs-wrapper">
+                <div className="jobs-container"> 
                     <a href="https://generalassemb.ly/instructors/elektra-babian/25723"
                     target="_blank">
                     <img id="ga-icon" src="https://i.imgur.com/JnZ5ttI.png"/>
@@ -33,10 +34,11 @@ const About = () => {
 
                 <div className="jobs-container">
                     <h5>Front-end Web Developer</h5>
-                </div> */}
+                </div>
             </div>
             {/* <Footer /> */}
-            </div>
+            
+        </div>
         </div>
         </div>
     );
