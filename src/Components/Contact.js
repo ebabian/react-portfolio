c166310e780dfefafa90206ac3f8885568b90e7d
ebabian <babianelektra@gmail.com>
2021-02-16 18:07:36 -0500

    Removed send btn from contact and changed text

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -11,14 +11,14 @@ const Contact = () => {
                 <img id="contactImg" src="https://i.imgur.com/WRsSm98.jpg" alt="Elektra Babian"/>
             </div>
             <div className="contact-bubble">
-                <p id="contact-p">To contact Elektra you can reach her with the email listed below. Thank you!</p>
+                <p id="contact-p">Get in Touch!</p>
                 <p id="email-p">babianelektra@gmail.com</p>
             </div>
             <div className="bubble"><p id="contact-social">Email</p></div>
             <div className="bubble2"><p id="contact-social">LinkedIn</p></div>
             <div className="bubble3"><p id="contact-social">GitHub</p></div>
             <div className="send-div">
-                <a id="send-btn" href="mailto: babianelektra@gmail.com">SEND</a>
+                {/* <a id="send-btn" href="mailto: babianelektra@gmail.com">SEND</a> */}
             </div>
             <Footer />
         </div>
