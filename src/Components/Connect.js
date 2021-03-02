9d289af0a07154060069c094e6dcc19d57a7c778
ebabian <babianelektra@gmail.com>
2021-03-01 21:26:37 -0500

    a tag link to id not working

--- src/Components/Connect.js
+++ src/Components/Connect.js
@@ -6,7 +6,7 @@ function Connect() {
     return (
         <div className="connect-container">
             <Fade bottom>
-            <a href="#eb">
+            <a href="/work/#eb">
                 <img 
                 id="up-arrow" 
                 alt="go up"
